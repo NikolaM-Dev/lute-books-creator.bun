ae3d6c98d270a20071b0bb7979dd47877a914718
Juan David Merchan Torres <juan1014166@gmail.com>
2024-10-14 14:20:23 -0500

    style(book): Change formatting

--- src/book.ts
+++ src/book.ts
@@ -2,6 +2,7 @@ export class Book {
   private static readonly BOOKS_PATH =
     '/home/nikola/Documents/english/lute/books';
   private static readonly RAW_BOOKS_PATH = './raw-books';
+
   static async bookExists(bookName: string): Promise<void> {
     const path = `${this.RAW_BOOKS_PATH}/${bookName}`;
     const file = Bun.file(path);
